c80a62e3d376d1ef0066c624a86618d155d75058
abasse-dev <69673423+abasse-dev@users.noreply.github.com>
2020-08-15 22:51:16 +0000

    fin de creation de la liste de telechargement

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const Color colorAmber = Colors.amber;
 const Color accentColor = Colors.blue;
 const Color colorWhite = Colors.white;
+const Color colorRed = Colors.red;
+const Color colorGreen = Colors.green;
--- lib/screens/download_screen.dart
+++ lib/screens/download_screen.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:projet_tafsir/components/drawer_component.dart';
 import 'package:projet_tafsir/constant/constant.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class DownloadScreen extends StatefulWidget {
   @override
   _DownloadScreenState createState() => _DownloadScreenState();
 }
 
 class _DownloadScreenState extends State<DownloadScreen> {
+  CollectionReference _firestore = Firestore.instance.collection('TAFSIR');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-         iconTheme: IconThemeData(color: Colors.white),
+        iconTheme: IconThemeData(color: Colors.white),
         title: Center(
           child: Text(
             "Téléchargement Tafsir",
@@ -21,6 +24,40 @@ class _DownloadScreenState extends State<DownloadScreen> {
         ),
       ),
       drawer: DrawerComponent(),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: _firestore.snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError)
+              return Text(
+                'une erreur de connexion !!',
+                style: TextStyle(color: colorRed),
+              );
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return Text(
+                  'Attendre cela peut prendre un peu de temps',
+                  style: TextStyle(color: colorGreen),
+                );
+                break;
+              default:
+                return ListView(
+                  children: snapshot.data.documents
+                      .map((document) => ListTile(
+                            title: Text(
+                              'Tafsir ${document['titre']}',
+                              style: TextStyle(color: accentColor,fontWeight: FontWeight.bold),
+                              
+                            ),
+                            leading: Icon(
+                              Icons.file_download,
+                              color: accentColor,
+                            ),
+                            onTap: (){},
+                          ))
+                      .toList(),
+                );
+            }
+          }),
     );
   }
 }
